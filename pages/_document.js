8b55d53f9fbfcad85fa84594f16d67e6d4d04f96
jmechristian <jmechristian@gmail.com>
2023-01-27 09:25:32 -0500

    gtyag

--- pages/_document.js
+++ pages/_document.js
@@ -16,19 +16,16 @@ export default function Document() {
           rel='stylesheet'
         />
         <Script
-          strategy='afterInteractive'
-          src='https://www.googletagmanager.com/gtag/js?id=G-02DBGHP71V'
-        />
-        <Script
-          id='google-analytics'
+          id='google-tag-manager'
           strategy='afterInteractive'
           dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-02DBGHP71V');`,
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PWJBGN2');`,
           }}
-        />
+        ></Script>
         <Script
           id='hotjar'
           strategy='afterInteractive'
@@ -45,6 +42,13 @@ export default function Document() {
         />
       </Head>
       <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PWJBGN2"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        ></noscript>
+
         <Main />
         <NextScript />
       </body>
