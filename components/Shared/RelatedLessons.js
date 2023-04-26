850363b56849a00b9126f6416ad535d110122c26
jmechristian <jmechristian@gmail.com>
2023-04-26 17:15:07 -0400

    xxxx

--- components/Shared/RelatedLessons.js
+++ components/Shared/RelatedLessons.js
@@ -32,7 +32,14 @@ const RelatedLessons = ({ relatedLessons, lessonId }) => {
                   <div className='max-w-xl'>
                     <div className='group relative'>
                       <h3 className='mt-6 text-xl font-greycliff font-semibold leading-6 text-gray-900 dark:text-base-brand group-hover:text-base-dark'>
-                        <a href={`/lessons/${course.slug}`}>
+                        <a
+                          href={`/lessons/${course.slug}`}
+                          onClick={() =>
+                            gtag('event', 'related_lesson_click', {
+                              lesson_title: course.title,
+                            })
+                          }
+                        >
                           <span className='absolute inset-0' />
                           {course.title}
                         </a>
