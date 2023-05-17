1d91c7656ac65971e13419d3cb1a0abbc4fd6e11
jmechristian <jmechristian@gmail.com>
2023-05-17 13:33:32 -0400

    ssss

--- pages/lessons/[id].js
+++ pages/lessons/[id].js
@@ -25,12 +25,15 @@ const Index = ({ lesson, lessons }) => {
   const [unlocked, setUnlocked] = useState(false);
   const [isPage, setIsPage] = useState(0);
 
+  const ogImage = lesson.seoImage;
+  console.log(ogImage);
+
   return (
     <>
       <Head>
         <title>{lesson.title}</title>
-        <meta property='og:image' content={lesson.seoImage} key='image' />
-        <meta property='og:title' content={lesson.title} key='title' />
+        <meta property='og:image' content={ogImage} key='image' />
+        <meta property='og:title' content='lesson' key='title' />
         <meta property='og:description' content={lesson.subhead} key='desc' />
         <meta name='description' content={lesson.subhead} key='desc' />
       </Head>
