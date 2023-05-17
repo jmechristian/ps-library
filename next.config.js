98c50566f4ff14382f0247b70bb7c41f8ce91b8e
jmechristian <jmechristian@gmail.com>
2023-05-17 14:07:40 -0400

    sss

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: {
     domains: ['packschool.s3.amazonaws.com', 'packagingschool.com'],
   },
