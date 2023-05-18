e09f1d9832cb8108390d57b9c53d24b21fd46b34
jmechristian <jmechristian@gmail.com>
2023-05-18 11:51:14 -0400

    aaa

--- components/SPA/SpaAbout.js
+++ components/SPA/SpaAbout.js
@@ -12,26 +12,6 @@ const SpaAbout = () => {
         <div className='font-semibold text-2xl text-slate-800 font-greycliff'>
           Be a Packaging Problem Solver
         </div>
-        <div className='flex gap-4'>
-          <div className='flex gap-1 items-center'>
-            <div>
-              <ComputerDesktopIcon className='w-5 h-5 stroke-white' />
-            </div>
-            <div className='font-medium text-slate-900'>Online</div>
-          </div>
-          <div className='flex gap-1 items-center'>
-            <div>
-              <HandRaisedIcon className='w-5 h-5 stroke-white' />
-            </div>
-            <div className='font-medium text-slate-900'>Hands-On</div>
-          </div>
-          <div className='flex gap-1 items-center'>
-            <div>
-              <ChatBubbleLeftEllipsisIcon className='w-5 h-5 stroke-white' />
-            </div>
-            <div className='font-medium text-slate-900'>Office Hours</div>
-          </div>
-        </div>
       </div>
       <p className='lg:text-lg lg:leading-relaxed text-slate-800 font-medium tracking-wide'>
         Master the fundementals of packaging design in this emmersive, online
