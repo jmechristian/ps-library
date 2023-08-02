65199c10381ed8db57e9c040d44484c96bc205c6
jmechristian <jmechristian@gmail.com>
2023-08-02 15:35:28 -0400

    ssss

--- components/SPA/MobilePackCamp.js
+++ components/SPA/MobilePackCamp.js
@@ -101,7 +101,7 @@ function Backdrop() {
 function CameraRig({ children }) {
   const group = useRef();
   useFrame((state, delta) => {
-    easing.damp3(state.camera.position, [0, -0.25, 2], 0.25, delta);
+    easing.damp3(state.camera.position, [0, -0.15, 1.75], 0.25, delta);
     easing.dampE(
       group.current.rotation,
       [state.pointer.y / 10, -state.pointer.x / 5, 0],
